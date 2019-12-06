f97bc69b4795d2d72aa470caaf4239c9862c8cef
alex <alexxustudy@gmail.com>
2019-12-06 15:38:38 +0800

    四皇后

--- leetCodeHard/queen.go
+++ leetCodeHard/queen.go
@@ -2,6 +2,13 @@ package main
 
 /**
  皇后问题
+给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+
+每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-queens
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 ***/
 func main() {
 	
@@ -9,7 +16,7 @@ func main() {
 var res [][]string
 
 func solveQueen(n int) [][]string  {
-	
+
 
 
 
