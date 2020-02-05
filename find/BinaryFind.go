2046a86dee5822825ff9ea2d2cc5b72a9b6c14c4
alex <alexxustudy@gmail.com>
2020-02-05 12:14:03 +0800

    fxk

--- find/BinaryFind.go
+++ find/BinaryFind.go
@@ -3,3 +3,23 @@ package main
 func main() {
 	
 }
+
+func mySqrt(x int) int {
+	if x == 1 {
+		return 1
+	}
+
+	left, right, res := 1, x, 0
+
+	for left <= right {
+		mid := left + (right - left) / 2
+		if mid <= x / mid {
+			left = mid + 1
+			res = mid
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return res
+}
